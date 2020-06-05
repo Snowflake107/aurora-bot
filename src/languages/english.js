127249d04eceb98a6eeac1c02b3d210d5aec4508
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-05 21:21:24 +0545

    Create english.js

---
+++ src/languages/english.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PREFIX_CHANGE: "Prefix set to {PREFIX}.",
+    HELP_DESC: "Use `a&help <number>` for Commands in the Category\n"+
+               "For Example, `{PREFIX}help 13` for Commands in Utility\n"+
+               "Total Commands: **{COMMANDS}**"
+};
