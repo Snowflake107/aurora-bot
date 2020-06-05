2bdac94dec8c9465324bcb964a57588080e2ba54
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-05 22:09:55 +0545

    Update english.js

--- src/languages/language/english.js
+++ src/languages/language/english.js
@@ -1,6 +1,6 @@
 module.exports = {
     PREFIX_CHANGE: "Prefix set to {PREFIX}.",
-    HELP_DESC: "Use `a&help <number>` for Commands in the Category\n"+
+    HELP_DESC: "Use `{PREFIX}help <number>` for Commands in the Category\n"+
                "For Example, `{PREFIX}help 13` for Commands in Utility\n"+
                "Total Commands: **{COMMANDS}**",
     HELP_MENU: "Help Menu",
