419f6a6c3864c5d2a14356cbf8389ceb56f6a981
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-05 21:57:51 +0545

    Numbers

--- src/languages/language/english.js
+++ src/languages/language/english.js
@@ -3,5 +3,15 @@ module.exports = {
     HELP_DESC: "Use `a&help <number>` for Commands in the Category\n"+
                "For Example, `{PREFIX}help 13` for Commands in Utility\n"+
                "Total Commands: **{COMMANDS}**",
-    TRANSLATION_ERROR: "Something went wrong in the translation system."
+    TRANSLATION_ERROR: "Something went wrong in the translation system.",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+    "0": "0"
 };
