7335c37234625b07d912dde8fd869995cff369fe
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-05 21:41:38 +0545

    Update english.js

--- src/languages/labguage/english.js
+++ src/languages/labguage/english.js
@@ -2,5 +2,6 @@ module.exports = {
     PREFIX_CHANGE: "Prefix set to {PREFIX}.",
     HELP_DESC: "Use `a&help <number>` for Commands in the Category\n"+
                "For Example, `{PREFIX}help 13` for Commands in Utility\n"+
-               "Total Commands: **{COMMANDS}**"
+               "Total Commands: **{COMMANDS}**",
+    TRANSLATION_ERROR: "Something went wrong in the translation system."
 };
